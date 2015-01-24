77b5836d7d83174f77aca931153f973602a5bdb8
Cristian Ibarra <cibar007@hammer.cs.ucr.edu>
2015-01-23 16:40:38 -0800

    changed rshell file to original

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,57 +1,75 @@
-nclude  <stdio.h>
-#include  <sys/types.h>
+nclude <iostream>
+#include <string>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <cctype>
+#include <functional>
+#include <locale>
+#include <sys/types.h>
 
-void  parse(char *line, char **argv)
+using namespace std;
+
+void makeARGV(char *cmd, char **argV)
 {
-     while (*line != '\0') {       /* if not the end of line ....... */ 
-          while (*line == ' ' || *line == '\t' || *line == '\n')
-               *line++ = '\0';     /* replace white spaces with 0    */
-          *argv++ = line;          /* save the argument position     */
-          while (*line != '\0' && *line != ' ' && 
-                 *line != '\t' && *line != '\n') 
-               line++;             /* skip the argument until ...    */
-     }
-     *argv = '\0';                 /* mark the end of argument list  */
+        while(*cmd != '\0')
+        {
+                if(*cmd == '#')
+                {
+                        *cmd = '\0';
+                        break;
+                }
+
+                while(*cmd == ' '|| *cmd == '\t' || *cmd == '\n')
+                        *cmd++ = '\0';
+
+                *argV++ = cmd;
+                while(*cmd != '\0' && *cmd != ' ' && *cmd != '\t' && *cmd != '\
+')
+                        cmd++;
+        }
+        *argV = '\0';
 }
 
-void  execute(char **argv)
+int main()
 {
-     pid_t  pid;
-     int    status;
+        char cmd[100];
+        /*
+ *         char sz[128] = "";
+ *
+ *                 string name = getlogin(sz, sizeof(sz));
+ *                         string host = gethostname(sz, sizeof(sz));*/
 
-     if ((pid = fork()) < 0) {     /* fork a child process           */
-          printf("*** ERROR: forking child process failed\n");
-          exit(1);
-     }
-     else if (pid == 0) {          /* for the child process:         */
-          if (execvp(*argv, argv) < 0) {     /* execute the command  */
-               printf("*** ERROR: exec failed\n");
-               exit(1);
-          }
-     }
-     else {                                  /* for the parent:      */
-          while (wait(&status) != pid)       /* wait for completion  */
-               ;
-     }
+        cout /*<< name << '@' << host */<< '$';
+        cin.getline(cmd, 100, '\n');
+        cout << endl;
+        char *argV[100];
+        makeARGV(cmd, argV);
+
+
+        int pid = fork();
+        if(pid == -1)
+        {
+                perror("fork fail");
+                exit(1);
+        }
+
+        else if(pid == 0)
+                if(execvp(*argV, argV) != 0)
+                {
+                        perror("error in execvp");
+                        exit(1);
+                }
+
+        else if(pid>0)
+        {
+                if(-1 == wait(0))
+                        perror("There was an error with wait().");
+        }
+	return 0;
 }
-
-void  main(void)
-{
-     char  line[1024];             /* the input line                 */
-     char  *argv[64];              /* the command line argument      */
-
-     while (1) {                   /* repeat until done ....         */
-          printf("Shell -> ");     /*   display a prompt             */
-          gets(line);              /*   read in the command line     */
-          printf("\n");
-          parse(line, argv);       /*   parse the line               */
-          if (strcmp(argv[0], "exit") == 0)  /* is it an "exit"?     */
-               exit(0);            /*   exit if it is                */
-          execute(argv);           /* otherwise, execute the command */
-     }
-return 0;
-}
-
 
 
 
